cb28a301d09de69ecc1199a0ac82908f643009e0
Cyroshadow <150596216+Cyroshadow@users.noreply.github.com>
2024-04-28 17:12:53 +0800

    Update Relay_Control.ino

--- Relay_Control.ino
+++ Relay_Control.ino
@@ -1,18 +1,51 @@
 void setup() {
-  pinMode(7, INPUT);
   pinMode(9, OUTPUT);
-  Serial.begin(9600);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+  
+  play_Siren(0, 0);
 }
 
 void loop() {
-  digitalWrite(9, LOW);
-  Serial.print("High ");
-  Serial.println(digitalRead(7));
-  delay(1000);
+  play_Siren(0, 0); //Turn off all sirens
+  play_Siren(1, 5000); //Play siren 1 sound for 5 seconds
+  play_Siren(2, 10000); //Play siren 2 sound for 10 seconds
+  play_Siren(3, 10000); //Play siren 3 sound for 10 seconds
   
-  digitalWrite(9, HIGH);
-  Serial.print("Low ");
-  Serial.println(digitalRead(7));
-  delay(1000);
   
+}
+
+void play_Siren(int severity, int length) {
+
+  switch (severity){
+
+    case 0:
+      digitalWrite(9, LOW);
+      digitalWrite(8, LOW);
+      digitalWrite(7, LOW);
+    break;
+
+    case 1:
+      digitalWrite(9, HIGH);
+      delay(length);
+      digitalWrite(9, LOW);
+    break;
+
+    case 2:
+      digitalWrite(8, HIGH);
+      digitalWrite(8, LOW);
+      delay(length);
+      digitalWrite(8, HIGH);
+      digitalWrite(8, LOW);
+    break;
+
+    case 3:
+      digitalWrite(7, HIGH);
+      digitalWrite(7, LOW);
+      delay(length);
+      digitalWrite(7, HIGH);
+      digitalWrite(7, LOW);
+    break;
+  }
+
 }
